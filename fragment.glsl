c42b36244b51ce87de7f57b07a9c4ea780cc8792
maxbittker <maxbittker@gmail.com>
2017-12-04 16:31:05 +0100

    [snapshot]

--- fragment.glsl
+++ fragment.glsl
@@ -5,9 +5,10 @@ uniform vec2 resolution;
 uniform sampler2D texture;
 uniform sampler2D webcam;
 uniform vec2 mouse;
-
 varying vec2 uv;
 float PI = 3.14159;
+vec2 pix = vec2(1./resolution.x,1./resolution.y);
+
 #pragma glslify: hsl2rgb = require(glsl-hsl2rgb) 
 #pragma glslify: luma = require(glsl-luma) 
 
@@ -15,10 +16,10 @@ float PI = 3.14159;
 #pragma glslify: gauss = require('glsl-specular-gaussian')
 #pragma glslify: camera = require('glsl-turntable-camera')
 #pragma glslify: noise4d = require('glsl-noise/simplex/4d')
-#pragma glslify: noise3d = require('glsl-noise/simplex/3d')
+// #pragma glslify: noise3d = require('glsl-noise/simplex/3d')
 #pragma glslify: noise2d = require('glsl-noise/simplex/2d')
 
-// #pragma glslify: fbm3d = require('glsl-fractal-brownian-noise/3d')
+#pragma glslify: fbm3d = require('glsl-fractal-brownian-noise/3d')
 // #pragma glslify: fbm4d = require('glsl-fractal-brownian-noise/4d')
 
 // #pragma glslify: noise4d = require(glsl-noise/simplex/4d)
@@ -33,14 +34,20 @@ void main () {
 
   vec2 sOffset = vec2(0, 1./resolution.y);
   sOffset *=0.0;
+  float a = fbm3d(vec3(uv*10.,t),9)*PI*2.;
+
+  sOffset = vec2(sin(a),cos(a))*pix;
+  sOffset.y -= pix.y;
   vec3 scolor = texture2D(texture, uv+sOffset).rgb;
   
-  vec3 color = scolor;
+  vec3 color = wcolor;
 
 
-  if( luma(wcolor) < luma(scolor) && sin(t*50.)>0.99){
-    color = wcolor;
+  if( luma(wcolor) > luma(scolor)){
+    color = scolor;
   }
+
+  color += vec3(0.005);
 
   if(length(scolor)< 0.01){
     color = wcolor;
