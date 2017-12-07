2fced3b84a42616af8448f35f2c0ee65d65f00ae
maxbittker <maxbittker@gmail.com>
2017-12-07 16:55:28 +0100

    [snapshot]

--- fragment.glsl
+++ fragment.glsl
@@ -29,7 +29,7 @@ void main () {
 
   vec3 wcolor = texture2D(webcam, sample).rgb;
   float wmag = luma(wcolor);
-  wcolor = hsl2rgb( fract(t*0.02) , 0.2, wmag+0.5);
+  wcolor = hsl2rgb( (sin(t*0.01)*0.5) +1.0 , 0.2, wmag+0.5);
  
 
   vec2 sOffset = vec2(0.0);
@@ -38,21 +38,18 @@ void main () {
 
   int n =5;
   float f = 0.01;
-  // vec2 q = vec2( fbm3d( vec3(uv,t * f) , n ),
-  //                    fbm3d( vec3(uv + vec2(5.2,1.3),t * f), n ) );
+  float uB = luma(texture2D(texture, uv+pix*vec2(0.,2.0)).rgb);
+  float dB = luma(texture2D(texture, uv+pix*vec2(0 ,-2.0)).rgb);
+  float lB = luma(texture2D(texture, uv+pix*vec2(-2.0,0.)).rgb);
+  float rB = luma(texture2D(texture, uv+pix*vec2(2.0,0.)).rgb);
+  
+  vec2 d = vec2(
+    rB - lB, dB - uB
+    // lB - rB, uB - dB
+  );
 
-  // vec2 p2 = vec2( fbm3d( vec3(uv + 4.0*q + vec2(1.7,9.2),t*f),n ),
-  //                    fbm3d( vec3(uv + 4.0*q + vec2(8.3,2.8),t*f),n ) );
-
-    // val = fbm3d( vec3(uv + 4.0*p2,t*f),n );
-    // float valcloud = fbm3d( vec3(uv + 4.0*p2,t*f),n );
-
-  // float a = fbm3d(vec3(uv*10.,t),9)*PI*2.;
-
-  // sOffset = vec2(sin(a),cos(a))*pix;
-  // sOffset.y -= pix.y;
-  // sOffset = p2 *pix*5.;
-  vec3 scolor = texture2D(texture, uv+sOffset).rgb;
+  vec3 scolor = texture2D(texture, uv + d*pix*20.  ).rgb;
+  // scolor = hsl2rgb( fract(t*0.02) , 0.2, luma(scolor)+0.5);
   
   vec3 color = wcolor;
   
@@ -66,10 +63,10 @@ void main () {
     color = wcolor;
   }
   // color += vec3(0.005);
-  // if(luma(color)> 0.7){
-    // color*=0.9;
+  if(luma(color)> 0.9){
+    // color*=0.5;
     // color+=wcolor*0.1;
-  // }
+  }
   
   gl_FragColor.rgb =color;
 
